ad5cef70d718cd2bbc8e64569c63e27ed58dfe94
jackszumowski1 <Jack.Szumowski1@marist.edu>
2019-02-11 17:35:42 -0500

    Solution to the mad libs exercise

---
+++ madlib.py
@@ -0,0 +1,17 @@
+#madlib.py
+#Description: Create a mad lib
+#Author: Jack Szumowski
+
+def main():
+
+    print("Today we are going to make a mad lib")
+    noun = input("First enter a noun: ")
+    verb = input("Second enter a verb: ")
+    adjective = input("Now an adjective: ")
+    place = input("Then enter a place: ")
+    
+    print("Here is your finished mad lib")
+    print("")
+    print("Take your", adjective, noun, "and", verb, "it at the", place, "!") 
+
+main()
