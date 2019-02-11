433bbf2f8193a306c46c002ef2179d70a12f6a8d
jackszumowski1 <Jack.Szumowski1@marist.edu>
2019-02-11 17:25:01 -0500

    Solution to evens loop program

---
+++ evens.py
@@ -0,0 +1,17 @@
+# evens.py
+# print out all the even numbers between 2 and 20 with a demo loop in front
+# Author: Jack Szumowski
+
+def main():
+
+    print("Here are the numbers 1-9")
+    
+    for i in range(10):
+         print(i)
+
+    print("Now here are the numbers 2-20 only evens")
+    
+    for i in range(2,21,2):
+        print(i)
+
+main()
